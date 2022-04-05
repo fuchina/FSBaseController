c76d76ed5cf680dda23939799fa00178a4756ee8
fuchina <1245102331@qq.com>
2022-04-05 17:57:36 +0800

    优化

--- FSBaseController/Classes/FSSelectController.h
+++ FSBaseController/Classes/FSSelectController.h
@@ -8,22 +8,30 @@
 
 #import "FSBaseController.h"
 
+@interface FSSelectModel : NSObject
+
+@property (nonatomic, assign) BOOL          selected;
+
+@property (nonatomic, copy) NSString        *content_id;            // 内容的id
+@property (nonatomic, copy) NSString        *content_show;          // 文字显示内容
+
++ (NSArray<FSSelectModel *> *)fastModelsWithTexts:(NSArray<NSString *> *)texts;
+
+@end
+
 @class FSSelectController;
-typedef void(^FSSelectControllerBlock)(FSSelectController *bSelectController,NSIndexPath *bIndexPath,NSArray *bArray);
+typedef void(^FSSelectControllerBlock)(FSSelectController *bSelectController, NSIndexPath *bIndexPath, NSArray<FSSelectModel *> *bArray);
 
 @interface FSSelectController : FSBaseController
 
-// 是否时是多选模式
-@property (nonatomic,assign) BOOL                           isMultiSelect;
+@property (nonatomic,strong) NSArray<FSSelectModel *>       *models;
 
-@property (nonatomic,strong) NSArray                        *array;
+@property (nonatomic,copy) void (^configCell)(UITableViewCell *bCell, NSIndexPath *bIndexPath, NSArray<FSSelectModel *> *bArray);
 
 // 单选的回调
 @property (nonatomic,copy) FSSelectControllerBlock          block;
 
 // 多选的回调
-@property (nonatomic,copy) void (^multiSelectCallback)(FSSelectController *bSelectController,NSArray<NSIndexPath *> *selecteds,NSArray *bArray);
-
-@property (nonatomic,copy) void (^configCell)(UITableViewCell *bCell,NSIndexPath *bIndexPath,NSArray *bArray);
+@property (nonatomic,copy) void (^multiSelectCallback)(FSSelectController *bSelectController, NSArray<FSSelectModel *> *bArray);
 
 @end
--- FSBaseController/Classes/FSSelectController.m
+++ FSBaseController/Classes/FSSelectController.m
@@ -8,29 +8,54 @@
 
 #import "FSSelectController.h"
 
+@implementation FSSelectModel
+
++ (NSArray<FSSelectModel *> *)fastModelsWithTexts:(NSArray<NSString *> *)texts {
+    if (![texts isKindOfClass:NSArray.class]) {
+        return nil;
+    }
+    NSMutableArray *list = [[NSMutableArray alloc] initWithCapacity:texts.count];
+    for (int x = 0; x < texts.count; x ++) {
+        NSString *txt = texts[x];
+        
+        FSSelectModel *m = [[FSSelectModel alloc] init];
+        m.content_id = txt;
+        m.content_show = txt;
+        [list addObject:m];
+    }
+    return list.copy;
+}
+
++ (NSArray<FSSelectModel *> *)selectedModelsFromModels:(NSArray<FSSelectModel *> *)models {
+    NSMutableArray *list = [[NSMutableArray alloc] init];
+    for (int x = 0; x < models.count; x ++) {
+        FSSelectModel *m = models[x];
+        if (m.selected) {
+            [list addObject:m];
+        }
+    }
+    return list.copy;
+}
+
+@end
+
 @interface FSSelectController ()<UITableViewDelegate,UITableViewDataSource>
 
 @end
 
-@implementation FSSelectController{
-    NSMutableArray  *_multis;
-}
+@implementation FSSelectController
 
-- (void)confirmSelected{
+- (void)confirmSelected {
     if (self.multiSelectCallback) {
-        self.multiSelectCallback(self, _multis, _array);
+        self.multiSelectCallback(self, [FSSelectModel selectedModelsFromModels:_models]);
     }
 }
 
 - (void)componentWillMount {
     [super componentWillMount];
         
-    if (_isMultiSelect) {
-        _multis = [[NSMutableArray alloc] init];
-        
-        UIBarButtonItem *bbi = [[UIBarButtonItem alloc] initWithTitle:@"确定" style:UIBarButtonItemStylePlain target:self action:@selector(confirmSelected)];
-        self.navigationItem.rightBarButtonItem = bbi;
-    }
+    UIBarButtonItem *bbi = [[UIBarButtonItem alloc] initWithTitle:@"确定" style:UIBarButtonItemStylePlain target:self action:@selector(confirmSelected)];
+    self.navigationItem.rightBarButtonItem = bbi;
     
     UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, WIDTHFC, HEIGHTFC) style:UITableViewStylePlain];
     tableView.delegate = self;
@@ -41,53 +66,43 @@
     [self.view addSubview:tableView];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return _array.count;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _models.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *identifier = @"cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
-    }
-    if (_configCell) {
-        _configCell(cell,indexPath,_array);
-    }else{        
-        cell.textLabel.text = _array[indexPath.row];
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
-    if (_isMultiSelect) {
-        if ([_multis containsObject:indexPath]) {
-            cell.accessoryType = UITableViewCellAccessoryCheckmark;
-        }else{
-            cell.accessoryType = UITableViewCellAccessoryNone;
-        }
+    if (_configCell) {
+        _configCell(cell,indexPath,_models);
+    }
+    
+    FSSelectModel *m = _models[indexPath.row];
+    cell.textLabel.text = m.content_show;
+        
+    if (m.selected) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
     }
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (_isMultiSelect) {
-        if (![_multis containsObject:indexPath]) {
-            [_multis addObject:indexPath];
-        }else{
-            [_multis removeObject:indexPath];
-        }
-        
-        [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
-    }else{
-        if (_block) {
-            _block(self,indexPath,_array);
-        }
+    
+    FSSelectModel *m = _models[indexPath.row];
+    m.selected = !m.selected;
+    [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+    
+    if (_block) {
+        _block(self, indexPath, [FSSelectModel selectedModelsFromModels:_models]);
     }
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 /*
