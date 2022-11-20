363285c0566c5c41c30aa5665a86aab0ceac8421
fuchina <1245102331@qq.com>
2022-11-20 18:40:03 +0800

    移入代码

---
+++ FSBaseController/Classes/FSConfigCOSController.h
@@ -0,0 +1,16 @@
+//
+//  FSConfigCOSController.h
+//  FSTututu
+//
+//  Created by 扶冬冬 on 2022/6/15.
+//
+
+#import "FSBaseController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FSConfigCOSController : FSBaseController
+
+@end
+
+NS_ASSUME_NONNULL_END
