69bf1ef27055c7bff353546caea2563310ba6880
fuchina <1245102331@qq.com>
2024-02-22 23:01:49 +0800

    2

---
+++ FSBaseController/Classes/FSSafeRootController.h
@@ -0,0 +1,16 @@
+//
+//  FSSafeRootController.h
+//  FSBaseController
+//
+//  Created by Dongdong Fu on 2024/2/22.
+//
+
+#import "FSBaseController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FSSafeRootController : FSBaseController
+
+@end
+
+NS_ASSUME_NONNULL_END
